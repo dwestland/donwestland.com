87ed40e8bf3a00994d26495cfffe3065a4e97453
dwestland <don@westland.net>
2022-06-07 08:04:56 -0700

    Update UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import React, { useEffect } from 'react'
-import { motion, useAnimation } from 'framer-motion'
-import { useInView } from 'react-intersection-observer'
+import { motion } from 'framer-motion'
 import './scss/main.scss'
 
 function App() {
-  const [ref, inView] = useInView()
-  const animation = useAnimation()
-
 
   const headerVariants = {
     offscreen: {
       opacity: 0,
       scale: 0,
-      // x: '100vw',
     },
     onscreen: {
       opacity: 1,
@@ -26,27 +21,24 @@ function App() {
     }
   }
 
-
   const sectionVariants = {
     offscreen: {
       opacity: 0,
-      x: 100
+      scale: 0.9,
+      x: 20
     },
     onscreen: {
       opacity: 1,
+      scale: 1,
       x: 0,
-      // rotate: -10,
       transition: {
-        type: "spring",
         bounce: 0.4,
-        duration: 0.8
+        duration: 1.5,
+        stifness: 150,
+        type: "spring",
       }
     }
   }
-
-
-
-
 
   const liVariants = {
     hover: {
@@ -58,10 +50,9 @@ function App() {
       }
     },
     tap: {
-      scale: .9,
+      scale: .95,
     }
   }
-
 
   useEffect(() => {
     console.log(`
@@ -80,36 +71,6 @@ Y88888888P     ee        .P      “Clean code always looks like it was
       '""Y888boodP""'
     `)
   }, [])
-// //////////////////////////////////// const inView: boolean
-// //////////////////////////////////// React Hook useEffect has a missing dependency: 'animation'. Either include it or remove the dependency array.eslintreact-hooks/exhaustive-deps
-
-    useEffect(() => {
-      if (inView) {
-        animation.start({
-          // scale: 0.5,
-          x: 0,
-          transition: {
-            
-            type: 'spring',
-            // stiffness: 150,
-            duration: 0.8,
-            bounce: 0.3,
-          },
-        })
-      
-      }
-
-      if(!inView) {
-        animation.start({
-          x: '-100vw',
-        })
-      }
-
-
-
-      console.log('%c InView ', 'background: red; color: white', inView)
-    }, [inView])
-    // }, [inView, animation])
   
   return (
     <div className="container">
@@ -118,19 +79,9 @@ Y88888888P     ee        .P      “Clean code always looks like it was
         variants={headerVariants}
         initial="hidden"
         animate="onscreen"
-
       >
         <img src="/images/tech-logos.jpg" alt="Technology Logos" />
       </motion.header>
-
-
-
-{/* 
-      <div className='row-spacer'>
-        &nbsp; 
-      </div> */}
-
-
       <div className='row preamble'>
         <motion.aside
           initial={{          
@@ -138,7 +89,6 @@ Y88888888P     ee        .P      “Clean code always looks like it was
             y: 0,
             opacity: 0,
             scale: 0.9,
-            // scale: firstKeyframeScale
           }}
           animate={{ 
             x: 0,
@@ -148,7 +98,6 @@ Y88888888P     ee        .P      “Clean code always looks like it was
           }}
           transition={{
             bounce: 1.3,
-            // delay: 1,
             stiffness: 300,
             type: 'spring',
           }}
@@ -156,39 +105,40 @@ Y88888888P     ee        .P      “Clean code always looks like it was
           <img src="/images/don-westland.jpg" alt="Don Westland" />
         </motion.aside>
         <motion.section
-          className="card-container"
           initial="offscreen"
           whileInView="onscreen"
           viewport={{ once: true, amount: 0.8 }}
         >
-          <div>
+          <motion.div variants={sectionVariants} >
+            <div>
 
-          </div>
-          <h1>Don Westland</h1>
-          <h2>Front End<br />Developer</h2>
-          <ul>
-            <motion.li
-              variants={liVariants}
-              whileHover={'hover'}
-              whileTap={'tap'}
-            >
-              Email: <a href = "mailto: don@westland.net">don@westland.net</a>
-            </motion.li>
-            <motion.li
-              variants={liVariants}
-              whileHover={'hover'}
-              whileTap={'tap'}
-            >
-              GitHub: <a href = "https://github.com/dwestland" target="_blank" rel="noreferrer">https://github.com/dwestland</a>
-            </motion.li>
-            <motion.li
-              variants={liVariants}
-              whileHover={'hover'}
-              whileTap={'tap'}
-            >
-              Resume: <a href="./westland-resume-2022.pdf" target="blank">westland-resume-2022.pdf</a>
-            </motion.li>
-          </ul>
+            </div>
+            <h1>Don Westland</h1>
+            <h2>Front End<br />Developer</h2>
+            <ul>
+              <motion.li
+                variants={liVariants}
+                whileHover={'hover'}
+                whileTap={'tap'}
+              >
+                Email: <a href = "mailto: don@westland.net">don@westland.net</a>
+              </motion.li>
+              <motion.li
+                variants={liVariants}
+                whileHover={'hover'}
+                whileTap={'tap'}
+              >
+                GitHub: <a href = "https://github.com/dwestland" target="_blank" rel="noreferrer">https://github.com/dwestland</a>
+              </motion.li>
+              <motion.li
+                variants={liVariants}
+                whileHover={'hover'}
+                whileTap={'tap'}
+              >
+                Resume: <a href="./westland-resume-2022.pdf" target="blank">westland-resume-2022.pdf</a>
+              </motion.li>
+            </ul>
+          </motion.div>
         </motion.section>
       </div>
       <div className='row-spacer'>
@@ -198,28 +148,34 @@ Y88888888P     ee        .P      “Clean code always looks like it was
         <aside>
           <img src="/images/internet-brands-logos.jpg" alt="Don Westland" />
         </aside>
-        <section>
-          <h2>Happy Path Video</h2>
-          <h3>Live Streaming Video from Venice Beach, CA</h3>
-          <div className="video-responsive">
-            <iframe
-              width="853"
-              height="480"
-              src={'https://www.youtube.com/embed/c4ogYaFSSQ4'}
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-              title="Embedded youtube"
-            />
-          </div>
-        </section>
+        <motion.section
+          initial="offscreen"
+          whileInView="onscreen"
+          viewport={{ once: true, amount: 0.8 }}
+        >
+          <motion.div variants={sectionVariants} >
+            <h2>Happy Path Video</h2>
+            <h3>Live Streaming Video from Venice Beach, CA</h3>
+            <div className="video-responsive">
+              <iframe
+                width="853"
+                height="480"
+                src={'https://www.youtube.com/embed/c4ogYaFSSQ4'}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="Embedded youtube"
+              />
+            </div>
+          </motion.div>
+        </motion.section>
       </div>
       <div className='row-spacer'>
         &nbsp; 
       </div>
       <div className='row'>
         <aside>
-          {/* <img src="/images/marine-corps-pics.jpg" alt="Don Westland" /> */}
+          {/* Should something be here? */}
         </aside>
         <motion.section
           initial="offscreen"
@@ -248,9 +204,14 @@ Y88888888P     ee        .P      “Clean code always looks like it was
       </div>
       <div className='row'>
         <aside>
-          {/* <img src="/images/marine-corps-pics.jpg" alt="Don Westland" /> */}
+          {/* Should something be here? */}
         </aside>
-        <section>
+        <motion.section
+          initial="offscreen"
+          whileInView="onscreen"
+          viewport={{ once: true, amount: 0.8 }}
+        >
+          <motion.div variants={sectionVariants} >
           <h2>Live Beach Cam</h2>
           <h3>Live Streaming Video from Venice Beach, CA</h3>
           <div className="video-responsive">
@@ -264,47 +225,49 @@ Y88888888P     ee        .P      “Clean code always looks like it was
               title="Embedded youtube"
             />
           </div>
-        </section>
+                  </motion.div>
+        </motion.section>
       </div>
       <div className='row-spacer'>
         &nbsp; 
       </div>
       <div className='row'>
-      <motion.aside
-
-        animate={animation}
-
-      >
+      <motion.aside>
           <img src="/images/westland-productions.jpg" alt="Don Westland" />
         </motion.aside>
-        <section>
-          <h2>Video Production</h2>
-          <h3>Cinematographer, Editor, Motion Graphics, Color &amp; Sound</h3>
-          <p><a href='https://www.westlandproductions.com/' target="_blank" rel="noreferrer">Westland Productions</a></p>
-          <p>Produce documentaries and travel videos. The trailer for Westland Traveler has over a million views.</p>
-        </section>
+        <motion.section
+          initial="offscreen"
+          whileInView="onscreen"
+          viewport={{ once: true, amount: 0.8 }}
+        >
+          <motion.div variants={sectionVariants} >
+            <h2>Video Production</h2>
+            <h3>Cinematographer, Editor, Motion Graphics, Color &amp; Sound</h3>
+            <p><a href='https://www.westlandproductions.com/' target="_blank" rel="noreferrer">Westland Productions</a></p>
+            <p>Produce documentaries and travel videos. The trailer for Westland Traveler has over a million views.</p>
+          </motion.div>
+        </motion.section>
       </div>
       <div className='row-spacer'>
         &nbsp; 
       </div>
-      <div className='row' ref={ref}>
-        <motion.aside
-
-          animate={animation}
-
-        >
+      <div
+        className='row'>
+        <motion.aside>
           <img src="/images/marine-corps-pics.jpg" alt="Marine Corps" />
         </motion.aside>
         <motion.section
-          // initial={{ x: '100vw' }}
-          // animate={{ x: 0 }}
-          // transition={{ type: 'spring', duration: 1, bounce: 0.3 }}
+          initial="offscreen"
+          whileInView="onscreen"
+          viewport={{ once: true, amount: 0.8 }}
         >
-          <h2>U.S.M.C.</h2>
-          <h3>United States Marine Corps Reserve</h3>
-          <p>
-          Helicopter Mechanic and Crew Chief<br />
-          E-4, Honorable Discharge</p>
+          <motion.div variants={sectionVariants} >
+            <h2>U.S.M.C.</h2>
+            <h3>United States Marine Corps Reserve</h3>
+            <p>
+            Helicopter Mechanic and Crew Chief<br />
+            E-4, Honorable Discharge</p>
+          </motion.div>
         </motion.section>
       </div>
       <div className='row-spacer'>
